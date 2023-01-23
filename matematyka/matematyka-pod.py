f895740d95040323e1d672bdc73fad17a52b0143
xiyao696 <krystian8222+github@gmail.com>
2023-01-23 20:10:13 +0100

    matematyka podstawowa
    suma, różnica, iloczyn, iloraz
    dla dwóch liczb dla liczb a i b

---
+++ matematyka/matematyka-pod.py
@@ -0,0 +1,12 @@
+a = int(input("Podaj wartosc a: "))
+b = int(input("Podaj wartosc b: "))
+
+suma = a + b
+roznica = a - b
+iloczyn = a * b
+iloraz = a / b
+
+print("Suma: ", suma)
+print("Roznica: ", roznica)
+print("Iloczyn: ", iloczyn)
+print("Iloraz: ", iloraz)

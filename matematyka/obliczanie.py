686d5023e0d1b9a2d1e6b2ba157a1140c23f912e
xiyao696 <krystian8222+github@gmail.com>
2023-03-09 17:39:52 +0100

    Create obliczanie.py

---
+++ matematyka/obliczanie.py
@@ -0,0 +1,10 @@
+print("POLA")
+
+a = int(input("Podaj wartosc a: "))
+b = int(input("Podaj wartosc b: "))
+h = int(input("Podaj wartosc h: "))
+
+if h is empty:
+    print("h is empty")
+else:
+    print("ERROR")
